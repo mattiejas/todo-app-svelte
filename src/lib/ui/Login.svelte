96d4713073e51d8225f5f9c482b87580e8a014f5
Matthias Aarnoutse <m.aarnoutse@sportradar.com>
2023-07-13 16:29:08 +0200

    Sign in with Discord

---
+++ src/lib/ui/Login.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { supabase } from '$lib/supabase';
+	import Button from '$lib/ui/Button.svelte';
+
+	async function signInWithDiscord() {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'discord',
+			options: {
+				redirectTo: window.location.origin + '/authorize'
+			}
+		});
+	}
+</script>
+
+<Button on:click={signInWithDiscord}>Login with Discord</Button>
---
+++ src/routes/authorize/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { supabase } from '$lib/supabase';
+
+	const user = supabase.auth.getUser();
+</script>
+
+{#await user}
+	<p>Waiting...</p>
+{:then value}
+	<p>The value is {JSON.stringify(value)}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
